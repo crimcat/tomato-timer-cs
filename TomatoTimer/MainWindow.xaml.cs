517567ca7960ff2ca4a716899c0b509f2720eff1
Stas Torgashov <crimcat@yandex.ru>
2017-05-04 18:25:56 +0300

    Добавьте файлы проекта.

---
+++ TomatoTimer/MainWindow.xaml.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading;
+using System.Windows;
+using System.IO;
+using System.Media;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace TomatoTimer {
+
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Create application main window
+        /// </summary>
+        public MainWindow() {
+            InitializeComponent();
+            StartClocks();
+            Application.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+
+            MemoryStream ms = new MemoryStream();
+            Resource.tb.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            bi.StreamSource = ms;
+            bi.EndInit();
+            helpImage.Source = bi;
+
+            trayNotifyIcon = new System.Windows.Forms.NotifyIcon();
+            trayNotifyIcon.Icon = System.Drawing.Icon.FromHandle(Resource.t.GetHicon());
+            trayNotifyIcon.Text = "TomatoTimer: Idle";
+            trayNotifyIcon.BalloonTipTitle = "TomatoTimer";
+            trayNotifyIcon.Click += delegate(object sender, EventArgs args) {
+                if(WindowState == WindowState.Normal) {
+                    Hide();
+                    WindowState = WindowState.Minimized;
+                } else {
+                    Show();
+                    WindowState = WindowState.Normal;
+                }
+            };
+            trayNotifyIcon.Visible = true;
+        }
+
+        /// <summary>
+        /// Define actions on application closing
+        /// </summary>
+        /// <param name="e">CancelEventArgs object reference</param>
+        protected override void OnClosing(CancelEventArgs e) {
+            trayNotifyIcon.Dispose();
+            trayNotifyIcon = null;
+            clocks.Abort();
+            base.OnClosing(e);
+        }
+
+        /// <summary>
+        /// Define actions when 'Go' button is clicked
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void GoButtonClick(object sender, RoutedEventArgs e) {
+            if (null == te) {
+                te = new TomatoTimer.TomatoEngine(bunchSize, tomatoDuration, breakDuration);
+                te.SetControlledIntervalElapsedCallback(() => {
+                    UpdateTimer();
+                });
+                te.SetEngineStateChangedCallback(() => {
+                    UpdateTimer();
+                    SetCurrentStateLabel();
+                    ComeToFront();
+                    trayNotifyIcon.ShowBalloonTip(1000);
+                });
+
+                te.Start();
+                UpdateTimer();
+                SetCurrentStateLabel();
+
+                ((Button)sender).IsEnabled = false;
+                pauseBtn.IsEnabled = true;
+                bunchSizeCombobox.IsEnabled = false;
+                tomatoDurationCombobox.IsEnabled = false;
+                breakDurationCombobox.IsEnabled = false;
+            } else {
+                throw new Exception("Logic problem with start button");
+            }
+        }
+
+        /// <summary>
+        /// Update timer-related user interface controls
+        /// </summary>
+        private void UpdateTimer() {
+            downcounterLabel.Dispatcher.BeginInvoke(new Action(() => {
+                downcounterLabel.Content = String.Format("0:{0:D2}", te.MinutesToGo);
+            }));
+            downtimerProgressBar.Dispatcher.BeginInvoke(new Action(() => {
+                downtimerProgressBar.Minimum = 0;
+                downtimerProgressBar.Maximum = te.CurrentStateDuration;
+                downtimerProgressBar.Value = te.MinutesToGo;
+            }));
+            bunchCounterLabel.Dispatcher.BeginInvoke(new Action(() => {
+                bunchCounterLabel.Content = String.Format("-{0}-", te.BunchSize);
+            }));
+            CreateTooltip();
+        }
+
+        /// <summary>
+        /// Make application main window on top on the screen
+        /// </summary>
+        private void ComeToFront() {
+            this.Dispatcher.BeginInvoke(new Action(() => {
+                if(WindowState == WindowState.Minimized) {
+                    Show();
+                    WindowState = WindowState.Normal;
+                }
+                Focus();
+            }));
+        }
+
+        /// <summary>
+        /// Play tomato started sound if configured
+        /// </summary>
+        private void PlayTomatoStartedSoundIfConfigured() {
+            if(isTomatoStartedSoundNeeded) {
+                soundPlayer.Stream = Resource.go_working;
+                soundPlayer.Play();
+            }
+        }
+
+        /// <summary>
+        /// Play break time started sound if configured
+        /// </summary>
+        private void PlayBreakStartedSoundIfConfigured() {
+            if(isBreakStartedSoundNeeded) {
+                soundPlayer.Stream = Resource._break;
+                soundPlayer.Play();
+            }
+        }
+
+        /// <summary>
+        /// Play tomatoes bunch exhaused sound if configured
+        /// </summary>
+        private void PlayBunchEndedSoundIfConfigured() {
+            if(isBunchEndedSoundNeeded) {
+                SoundPlayer sp = new SoundPlayer(Resource.finish);
+                soundPlayer.Stream = Resource.finish;
+                soundPlayer.Play();
+            }
+        }
+
+        /// <summary>
+        /// Adaptive create current tool tip text for tray icon
+        /// </summary>
+        private void CreateTooltip() {
+            switch(te.CurrentState) {
+            case TomatoEngine.State.IDLE:
+                trayNotifyIcon.Text = "TomatoTmer: idle";
+                trayNotifyIcon.BalloonTipText = "Idle";
+                break;
+            case TomatoEngine.State.WORKING:
+                trayNotifyIcon.Text = String.Format("TomatoTimer: working - {0} min left", te.MinutesToGo);
+                trayNotifyIcon.BalloonTipText = "Working";
+                break;
+            case TomatoEngine.State.BREAK:
+                trayNotifyIcon.Text = String.Format("TomatoTimer: break - {0} min left", te.MinutesToGo);
+                trayNotifyIcon.BalloonTipText = "Break";
+                break;
+            case TomatoEngine.State.PAUSED:
+            case TomatoEngine.State.PAUSED_IN_BREAK:
+                trayNotifyIcon.Text = "TomatoTimer: paused";
+                trayNotifyIcon.BalloonTipText = "Paused";
+                break;
+            case TomatoEngine.State.FINISHED:
+                trayNotifyIcon.Text = "TomatoTimer: finished";
+                trayNotifyIcon.BalloonTipText = "Finished";
+                break;
+            }
+        }
+
+        /// <summary>
+        /// Adaptive create current state title label
+        /// </summary>
+        private void SetCurrentStateLabel() {
+            string message = "У нас сейчас: ждём команды";
+            switch(te.CurrentState) {
+            case TomatoEngine.State.IDLE:
+                break;
+            case TomatoEngine.State.WORKING:
+                message = "У нас сейчас: помидорка";
+                PlayTomatoStartedSoundIfConfigured();
+                break;
+            case TomatoEngine.State.BREAK:
+                message = "У нас сейчас: перерыв";
+                PlayBreakStartedSoundIfConfigured();
+                break;
+            case TomatoEngine.State.PAUSED_IN_BREAK:
+            case TomatoEngine.State.PAUSED:
+                message = "У нас сейчас: пауза";
+                break;
+            case TomatoEngine.State.FINISHED:
+                message = "У нас сейчас: отсчёт закончен";
+                PlayBunchEndedSoundIfConfigured();
+                bunchSizeCombobox.Dispatcher.BeginInvoke(new Action(() => {
+                    bunchSizeCombobox.IsEnabled = false;
+                }));
+                tomatoDurationCombobox.Dispatcher.BeginInvoke(new Action(() => {
+                    tomatoDurationCombobox.IsEnabled = false;
+                }));
+                breakDurationCombobox.Dispatcher.BeginInvoke(new Action(() => {
+                    breakDurationCombobox.IsEnabled = false;
+                }));
+                break;
+            }
+            currentStateLabel.Dispatcher.BeginInvoke(new Action(() => {
+                currentStateLabel.Content = message;
+                CreateTooltip();
+                trayNotifyIcon.ShowBalloonTip(0);
+            }));
+        }
+
+        /// <summary>
+        /// Define actions when 'Pause' button is clicked
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void PauseButtonClick(object sender, RoutedEventArgs e) {
+            if(null != te) {
+                switch (((Button)sender).Content) {
+                case "Пауза":
+                    ((Button)sender).Content = "Дальше";
+                    te.Pause();
+                    break;
+                case "Дальше":
+                    ((Button)sender).Content = "Пауза";
+                    te.Proceed();
+                    break;
+                }
+                SetCurrentStateLabel();
+            }
+        }
+
+        /// <summary>
+        /// Define actions when 'Finish' application button is clicked
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void FinishApplicationButtonClick(object sender, RoutedEventArgs e) {
+            if(null != te) {
+                te.Cancel();
+            }
+            clocks.Abort();
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// Define actions when bunch size combobox selection is made
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void BunchSizeSelectorChanged(object sender, RoutedEventArgs e) {
+            bunchSize = UInt16.Parse((string)((ComboBoxItem)sender).Content);
+        }
+
+        /// <summary>
+        /// Define actions when tomato duration combobox selection is made
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void TomatoDurationSelectorChanged(object sender, RoutedEventArgs e) {
+            tomatoDuration = UInt16.Parse((string)((ComboBoxItem)sender).Content);
+        }
+
+        /// <summary>
+        /// Define actions when break time duration combobox selection is made
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void BreakDurationSelectorChanged(object sender, RoutedEventArgs e) {
+            breakDuration = UInt16.Parse((string)((ComboBoxItem)sender).Content);
+        }
+
+        /// <summary>
+        /// Define action when tomato sound needed checkbox is clicked
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void TomatoStartedSoundNeeded(object sender, RoutedEventArgs e) {
+            bool? flag = ((CheckBox)sender).IsChecked;
+            if(flag.HasValue) {
+                isTomatoStartedSoundNeeded = (bool)flag;
+            } else {
+                isTomatoStartedSoundNeeded = false;
+            }
+        }
+
+        /// <summary>
+        /// Define action when break sound needed checkbox is clicked
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void BreakStartedSoundNeeded(object sender, RoutedEventArgs e) {
+            bool? flag = ((CheckBox)sender).IsChecked;
+            if(flag.HasValue) {
+                isBreakStartedSoundNeeded = (bool)flag;
+            } else {
+                isBreakStartedSoundNeeded = false;
+            }
+        }
+
+        /// <summary>
+        /// Define action when bunch ended sound needed checkbox is clicked
+        /// </summary>
+        /// <param name="sender">action sender object reference</param>
+        /// <param name="e">RoutedEventArgs object reference</param>
+        private void BunchEndedSoundNeeded(object sender, RoutedEventArgs e) {
+            bool? flag = ((CheckBox)sender).IsChecked;
+            if(flag.HasValue) {
+                isBunchEndedSoundNeeded= (bool)flag;
+            } else {
+                isBunchEndedSoundNeeded = false;
+            }
+        }
+
+        /// <summary>
+        /// Start autonomous clocks to be shown on application mail panel
+        /// </summary>
+        private void StartClocks() {
+            clocks = new Thread(() => {
+                while(true) {
+                    TimeSpan now = DateTime.Now.TimeOfDay;
+                    clocksLabel.Dispatcher.BeginInvoke(new Action(() => {
+                        clocksLabel.Content = String.Format("{0:D2}:{1:D2}", now.Hours, now.Minutes);
+                    }));
+                    Thread.Sleep(1000 * (60 - now.Seconds));
+                }
+            });
+            clocks.Start();
+        }
+
+        private uint bunchSize = 2;
+        private uint tomatoDuration = 30;
+        private uint breakDuration = 5;
+
+        private bool isTomatoStartedSoundNeeded = false;
+        private bool isBreakStartedSoundNeeded = false;
+        private bool isBunchEndedSoundNeeded = false;
+
+        private TomatoTimer.TomatoEngine te = null;
+
+        private SoundPlayer soundPlayer = new SoundPlayer();
+        private System.Windows.Forms.NotifyIcon trayNotifyIcon = null;
+        private Thread clocks = null;
+    }
+}
---
+++ TomatoTimer/TomatoEngine.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TomatoTimer {
+
+    /// <summary>
+    /// Minutes counter with callbacks: callback on each minute and callback on all time period finished
+    /// </summary>
+    class MinutesTimer {
+
+        public delegate void OneMinutePassed();
+        public delegate void TimeCounterFinished();
+
+        /// <summary>
+        /// Create minutes counter with two callback. Callbacks are mandatory, cannot be null.
+        /// </summary>
+        /// <param name="minutesNotifier"></param>
+        /// <param name="timeFinishedNotifier"></param>
+        internal MinutesTimer(OneMinutePassed minutesNotifier, TimeCounterFinished timeFinishedNotifier) {
+            if((null == minutesNotifier) || (null == timeFinishedNotifier)) {
+                throw new ArgumentNullException();
+            }
+            this.minutesNotifier = minutesNotifier;
+            this.timeFinishedNotifier = timeFinishedNotifier;
+        }
+
+        /// <summary>
+        /// Start counter for the given number of minutes.
+        /// </summary>
+        /// <param name="minutes"></param>
+        public void Go(uint minutes) {
+            if(MinutesLeft == 0) {
+                MinutesLeft = minutes;
+                worker = new Thread(() => {
+                    int gransCounter = 0;
+                    while(Thread.CurrentThread.IsAlive) {
+                        Thread.Sleep(GRANULARITY_IN_SECONDS * 1000);
+                        if(!isPaused) {
+                            gransCounter += GRANULARITY_IN_SECONDS;
+                            if(gransCounter >= ONE_MINUTE) {
+                                gransCounter = 0;
+                                if(DecrementMinutesCounter()) {
+                                    timeFinishedNotifier();
+                                    break;
+                                }
+                                minutesNotifier();
+                            }
+                        }
+                    }
+                });
+                worker.Start();
+            }
+        }
+
+        /// <summary>
+        /// Pause minutes counter. If it wasn't started before the method does nothing.
+        /// </summary>
+        public void Pause() {
+            if(MinutesLeft > 0) {
+                isPaused = true;
+            }
+        }
+
+        /// <summary>
+        /// Start minutes counter agian. If it wasn't started and paused before the method does nothing.
+        /// </summary>
+        public void Proceed() {
+            if(isPaused && (MinutesLeft > 0)) {
+                isPaused = false;
+            }
+        }
+
+        /// <summary>
+        /// Cancel currently running (or being paused) minutes counter.
+        /// </summary>
+        public void Cancel() {
+            if((null != worker) && worker.IsAlive) {
+                isPaused = false;
+                worker.Abort();
+            }
+            worker = null;
+        }
+
+        /// <summary>
+        /// Property: how many minutes are still left to run.
+        /// </summary>
+        public uint MinutesLeft {
+            get { return minutesLeft; }
+            private set { minutesLeft = value; }
+        }
+
+        /// <summary>
+        /// Decrement number of minutes left.
+        /// </summary>
+        /// <returns>Return true if counter finished</returns>
+        private bool DecrementMinutesCounter() {
+            if(MinutesLeft > 0) {
+                MinutesLeft = MinutesLeft - 1;
+            }
+            return (0 == MinutesLeft);
+        }
+
+        private const int GRANULARITY_IN_SECONDS = 2;
+        private const int ONE_MINUTE = 60;
+
+        private uint minutesLeft = 0;
+        private bool isPaused = false;
+
+        private Thread worker = null;
+
+        private OneMinutePassed minutesNotifier;
+        private TimeCounterFinished timeFinishedNotifier;
+    }
+
+    public class TomatoEngine {
+
+        /// <summary>
+        /// Engine state changed callback.
+        /// </summary>
+        public delegate void EngineStateChanged();
+        /// <summary>
+        /// Time interval (1 minute) is passed callback
+        /// </summary>
+        public delegate void ControlledIntervalElapsed();
+
+        /// <summary>
+        /// Possible engine states
+        /// </summary>
+        public enum State {
+            /// <summary>
+            /// Engine is not running.
+            /// </summary>
+            IDLE,
+            /// <summary>
+            /// Engine is running and we're in work time interval
+            /// </summary>
+            WORKING,
+            /// <summary>
+            /// Engine is running and we're in break time interval
+            /// </summary>
+            BREAK,
+            /// <summary>
+            /// Engine is not running, time counter has been finished
+            /// </summary>
+            FINISHED,
+            /// <summary>
+            /// Engine is running, we're in work time interval and time counter is paused
+            /// </summary>
+            PAUSED,
+            /// <summary>
+            /// Engine is running, we're in break time interval and time counter is paused
+            /// </summary>
+            PAUSED_IN_BREAK
+        }
+
+        /// <summary>
+        /// Create engine object with given parameters.
+        /// </summary>
+        /// <param name="bunchSize">Number of tomatos in the bunch</param>
+        /// <param name="tomatoDuration">Work time period (tomato) duration in minutes</param>
+        /// <param name="breakDuration">Break time period duration in minutes</param>
+        public TomatoEngine(uint bunchSize, uint tomatoDuration, uint breakDuration) {
+            this.bunchSize = bunchSize;
+            this.tomatoDuration = tomatoDuration;
+            this.breakDuration = breakDuration;
+
+            this.minutesTimer = new MinutesTimer(
+                () => { controlledIntervalElapsed(); },
+                () => {
+                    CheckUpdateBunchCounter();
+                    currentState = getNextState();
+                    minutesTimer.Go(CurrentStateDuration);
+                    engineStateChanged();
+                }
+            );
+        }
+
+        /// <summary>
+        /// Set callback for engine start change
+        /// </summary>
+        /// <param name="engineStateChanged">Engine state change delegate</param>
+        /// <returns></returns>
+        public TomatoEngine SetEngineStateChangedCallback(EngineStateChanged engineStateChanged) {
+            this.engineStateChanged = engineStateChanged;
+            return this;
+        }
+
+        /// <summary>
+        /// Set callback for engine controlled inverval time passed (1 minutes)
+        /// </summary>
+        /// <param name="controlledIntervalElapsed">engine controlled interval delegate</param>
+        /// <returns></returns>
+        public TomatoEngine SetControlledIntervalElapsedCallback(ControlledIntervalElapsed controlledIntervalElapsed) {
+            this.controlledIntervalElapsed = controlledIntervalElapsed;
+            return this;
+        }
+
+        /// <summary>
+        /// Get remaining tomatoes (current bunch size), in other words - number of tomatoes left
+        /// </summary>
+        public uint BunchSize {
+            get { return bunchSize; }
+        }
+        /// <summary>
+        /// Get work time (tomato) duration in minutes
+        /// </summary>
+        public uint TomatoDuration {
+            get { return tomatoDuration; }
+        }
+        /// <summary>
+        /// Get break time duration in minutes
+        /// </summary>
+        public uint BreakDuration {
+            get { return breakDuration; }
+        }
+        /// <summary>
+        /// Get duration for current state, returns null for states which do not have duration defined
+        /// </summary>
+        public uint CurrentStateDuration {
+            get {
+                switch(currentState) {
+                case State.WORKING: return TomatoDuration;
+                case State.BREAK: return BreakDuration;
+                }
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Get number of minutes left for the timer (current state)
+        /// </summary>
+        public uint MinutesToGo {
+            get { return minutesTimer.MinutesLeft; }
+        }
+        /// <summary>
+        /// Get engine current state.
+        /// <see cref="State"/>
+        /// </summary>
+        public State CurrentState { get { return currentState; } }
+
+        /// <summary>
+        /// Start engine
+        /// </summary>
+        public void Start() {
+            if(State.IDLE == currentState) {
+                currentState = State.WORKING;
+                minutesTimer.Go(CurrentStateDuration);
+            }
+        }
+
+        /// <summary>
+        /// Pause engine
+        /// </summary>
+        public void Pause() {
+            if(State.WORKING == currentState) {
+                currentState = State.PAUSED;
+                minutesTimer.Pause();
+            }
+        }
+
+        /// <summary>
+        /// Start paused engine again
+        /// </summary>
+        public void Proceed() {
+            if(State.PAUSED == currentState) {
+                currentState = State.WORKING;
+                minutesTimer.Proceed();
+            }
+        }
+
+        /// <summary>
+        /// Cancel current engine. Time counter is dropped.
+        /// </summary>
+        public void Cancel() {
+            if(State.IDLE != currentState) {
+                currentState = State.FINISHED;
+                minutesTimer.Cancel();
+            }
+        }
+
+        /// <summary>
+        /// Decrement bunch counter if it is possible
+        /// </summary>
+        private void CheckUpdateBunchCounter() {
+            if((State.BREAK == currentState) && (State.WORKING == getNextState())) {
+                --bunchSize;
+            }
+        }
+
+        /// <summary>
+        /// Calculate next engine state
+        /// </summary>
+        /// <returns>New state the engine can switch to</returns>
+        private State getNextState() {
+            switch(currentState) {
+            case State.IDLE: return State.IDLE;
+            case State.WORKING: return State.BREAK;
+            case State.BREAK: return BunchSize > 0 ? State.WORKING : State.FINISHED;
+            case State.FINISHED: return State.FINISHED;
+            case State.PAUSED: return State.WORKING;
+            case State.PAUSED_IN_BREAK: return State.BREAK;
+            }
+            return State.IDLE;
+        }
+        private State currentState = State.IDLE;
+
+        private uint bunchSize;
+        private uint tomatoDuration;
+        private uint breakDuration;
+
+        private MinutesTimer minutesTimer;
+
+        private EngineStateChanged engineStateChanged;
+        private ControlledIntervalElapsed controlledIntervalElapsed;
+    }
+}
